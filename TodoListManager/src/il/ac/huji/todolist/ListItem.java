c1447fd06279976c21d0232f1e3691ef6cc8e5ef
lior bar <lior656@gmail.com>
2013-03-24 20:37:27 +0200

    now instractions start

--- TodoListManager/src/il/ac/huji/todolist/ListItem.java
+++ TodoListManager/src/il/ac/huji/todolist/ListItem.java
@@ -2,7 +2,7 @@ package il.ac.huji.todolist;
 
 import java.util.Date;
 
-public class ListItem {
+public class ListItem implements ITodoItem{
 	public String _todo_title;
 	public Date _todo_date;
 	
@@ -10,6 +10,18 @@ public class ListItem {
 	public ListItem(String addStr, Date todo_date) {
 		_todo_title = addStr;
 		_todo_date = todo_date;
+	}
+
+
+	@Override
+	public String getTitle() {
+		return _todo_title;
+	}
+
+
+	@Override
+	public Date getDueDate() {
+		return _todo_date;
 	}	
 
 }
---
+++ TodoListManager/src/il/ac/huji/todolist/TodoDAL.java
@@ -0,0 +1,15 @@
+package il.ac.huji.todolist;
+
+public class TodoDAL {
+	public TodoDAL(Context context) { 
+
+	}
+	public boolean insert(ITodoItem todoItem) {
+	}
+	public boolean update(ITodoItem todoItem) { 
+	}
+	public boolean delete(ITodoItem todoItem) { 
+	}
+	public List<ITodoItem> all() {
+	}
+}
